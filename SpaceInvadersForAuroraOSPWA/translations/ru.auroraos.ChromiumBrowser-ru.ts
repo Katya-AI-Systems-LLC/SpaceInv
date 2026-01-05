813d072165a689721e278d25217e8115f552166a
Dmitry Olegovich Sorokin (@sorydima) <43252008+sorydima@users.noreply.github.com>
2026-01-05 16:32:44 +0300

    diff ..

---
+++ SpaceInvadersForAuroraOSPWA/translations/ru.auroraos.ChromiumBrowser-ru.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="24"/>
+        <source>About Application</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="38"/>
+        <source>&lt;p&gt;Space Invaders Enhanced Edition превращает классическую аркадную игру в современный, многофункциональный космический шутер с более чем 50 улучшениями, включая усовершенствованные системы вооружения, специальные способности, опасности окружающей среды и динамическое масштабирование сложности.&lt;/p&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="43"/>
+        <source>The 3-Clause BSD License</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="57"/>
+        <source>&lt;p&gt;&lt;em&gt;© Copyright (c) 2026 Space Invaders Team.&lt;/em&gt;&lt;/p&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WebViewBrowser</name>
+    <message>
+        <location filename="../qml/WebViewBrowser.qml" line="8"/>
+        <source>SpaceInvaders</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ SpaceInvadersForAuroraOSPWA/translations/ru.auroraos.ChromiumBrowser.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="24"/>
+        <source>About Application</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="38"/>
+        <source>&lt;p&gt;Space Invaders Enhanced Edition transforms the classic arcade game into a modern, feature-rich space shooter with 50+ enhancements including advanced weapon systems, special abilities, environmental hazards, and dynamic difficulty scaling.&lt;/p&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="43"/>
+        <source>The 3-Clause BSD License</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="57"/>
+        <source>&lt;p&gt;&lt;em&gt;© Copyright (c) 2026 Space Invaders Team.&lt;/em&gt;&lt;/p&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WebViewBrowser</name>
+    <message>
+        <location filename="../qml/WebViewBrowser.qml" line="8"/>
+        <source>SpaceInvaders</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
